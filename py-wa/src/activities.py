6e1654076ff9ac505fe4918afb02f7a707dc4a93
ashish rana <dev.livelifelively@gmail.com>
2025-02-11 20:37:04 +0530

    python activity with type

--- py-wa/src/activities.py
+++ py-wa/src/activities.py
@@ -1,8 +1,25 @@
 import asyncio
 from temporalio import activity
+from typing import Any, TypedDict, List, Optional
+
+class ParliamentQuestion(TypedDict):
+    quesNo: int
+    subjects: str
+    lokNo: str
+    member: List[str]
+    ministry: str
+    type: str
+    date: str
+    questionsFilePathLocal: str
+    questionsFilePathWeb: str
+    questionsFilePathHindiLocal: Optional[str]
+    questionsFilePathHindiWeb: Optional[str]
+    questionText: Optional[str]
+    answerText: Optional[str]
+    sessionNo: Optional[str]
 
 @activity.defn
-async def pyActivity(input_string: str) -> str:
-    print(f"Running Python activity with input: {input_string}")
+async def pyActivity(input_data: List[ParliamentQuestion]) -> Any:
+    print(f"Running Python activity with input: {input_data}")
     await asyncio.sleep(0.5) # Simulate processing
-    return f"Python: {input_string}"
+    return input_data   
